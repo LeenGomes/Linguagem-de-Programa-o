3037ee3ce96e95f29f971a8a297582db194356e5
Kathleen <kathleen.gomesdacosta@gmail.com>
2025-03-28 21:58:40 -0300

    Linguagem de Programação

---
+++ LP_Lista_02/LP_Lista_02/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args) {
+            int number, result;
+
+            System.out.printIn("Digite um número: ");
+
+            Scanner in = new Scanner(System.in); // Configura o Scanner
+            number = in.nextInt();
+
+            result = number * 2;
+            System.out.printIn("O dobro de " + number + "é " + result)
+    }
+}
